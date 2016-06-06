a0af20eac6c75b40d77e500b5fd7ce1bf9300184
Leonardo Farroco <leonardo.farroco@gmail.com>
2016-06-06 05:06:36 -0300

    chart draft

---
+++ src/js/dfe.chart.js
@@ -0,0 +1,56 @@
+(function(){
+
+	'use strict';
+
+	var chart = {
+
+		data : {
+		}
+
+	};
+
+	chart.data.politica= {views:600,name:"POLÍTICA",perc:0};
+	chart.data.economia = {views:500,name:"ECONOMIA",perc:0};
+	chart.data.ciencia = {views:400,name:"CIÊNCIA",perc:0};
+	chart.data.brasil = {views:300,name:"BRASIL",perc:0};
+	chart.data.mundo = {views:200,name:"MUNDO",perc:0};
+	chart.data.cultura = {views:100,name:"CULTURA",perc:0};
+
+	chart.largest = 0;
+
+	for(var row in chart.data){
+
+		if(chart.data[row].views > chart.largest)
+			chart.largest = chart.data[row].views;
+	
+	}
+
+	for(var row in chart.data){
+
+		chart.data[row].perc = (chart.data[row].views / chart.largest ) * 100;
+
+		$('#chart').append(
+'<div class="row"><div class="col two">'+chart.data[row].name+'</div><div class="col eight">'+
+'<div id="chart-'+row+'" class="chart">'+chart.data[row].views+'</div></div>'+
+'</div></div>'
+
+		);
+	
+	}
+
+	setTimeout(function(){
+
+		for(var row in chart.data){
+	
+			$('#chart-'+row).css('width',chart.data[row].perc+'%');
+	
+		}
+
+	},2000);
+
+	
+
+
+	dfe.chart = chart;
+	
+})();
